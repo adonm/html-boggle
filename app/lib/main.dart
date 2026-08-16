f9c54b59ee2ff83d528bee05e7553ae8cf38a034
Adon Metcalfe <adonm@fastmail.fm>
2026-08-16 15:48:56 +0800

    refactor: move debug hooks into debug_hooks.dart
    main.dart is now just bootstrapping: init the game, install the e2e
    hooks, run the themed app. All __boggleDebug* plumbing lives in
    debug_hooks.dart.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,103 +1,10 @@
-import 'dart:convert';
-import 'dart:js_interop';
-import 'dart:js_interop_unsafe';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 
+import 'debug_hooks.dart';
 import 'game.dart';
 import 'screens.dart';
 import 'theme.dart';
-
-@JS('window')
-external JSObject get _window;
-
-/// Install test/debug hooks on window for the e2e harness.
-void _installDebugHooks(Game game) {
-  final state = ((JSString _) => jsonEncode(game.debugState()).toJS).toJS;
-  _window.setProperty('__boggleDebugState'.toJS, state);
-  _window.setProperty(
-    '__boggleDebugJoin'.toJS,
-    ((JSString room, JSString name) {
-      game.join(roomCode: room.toDart, name: name.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugSubmit'.toJS,
-    ((JSString word) {
-      game.boggle?.debugSubmit(word.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugStart'.toJS,
-    ((JSAny _) {
-      game.startRound();
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugReady'.toJS,
-    ((JSAny _) {
-      game.toggleReady();
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugSubmitScat'.toJS,
-    ((JSString word) {
-      game.scatter?.submit(word.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugEndRound'.toJS,
-    ((JSAny _) {
-      game.debugEndRound();
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugSetMode'.toJS,
-    ((JSString mode) {
-      game.debugSetMode(mode.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugGuess'.toJS,
-    ((JSString text) {
-      game.sketch?.guess(text.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugChessMove'.toJS,
-    ((JSString from, JSString to) {
-      game.chess?.tryMove(from.toDart, to.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugGoMove'.toJS,
-    ((JSString coord) {
-      game.go?.tryMove(coord.toDart);
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugGoPass'.toJS,
-    ((JSAny _) {
-      game.go?.passTurn();
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugWtPlay'.toJS,
-    ((JSString tilesJson) {
-      final raw = jsonDecode(tilesJson.toDart);
-      if (raw is List) {
-        game.wt?.tryPlay([for (final t in raw) if (t is List) t]);
-      }
-    }).toJS,
-  );
-  _window.setProperty(
-    '__boggleDebugWtPass'.toJS,
-    ((JSAny _) {
-      game.wt?.passTurn();
-    }).toJS,
-  );
-}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -106,7 +13,7 @@ Future<void> main() async {
 
   final game = Game();
   await game.init();
-  _installDebugHooks(game);
+  installDebugHooks(game);
 
   final themes = ThemeController.instance;
 
